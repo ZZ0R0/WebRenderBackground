22d4cf094b6578c0127773292fb487cf98993ae3
ZZ0R0 <84625804+ZZ0R0@users.noreply.github.com>
2023-12-11 17:34:53 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt-get install -y qt5-qmake qtwebengine5-dev
-cd ./engine
-qmake
-make
-echo -e "\nRun ./project to start the project\n"
-ls
+sudo apt-get install -y qt5-qmake qtwebengine5-dev;
+cd ./engine;
+qmake;
+make;
+echo -e "\nRun ./project to start the project\n";
+ls;
